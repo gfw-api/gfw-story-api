0784b9da96ad98c02f89d773e0bccc5c41fa3816
Adam Mulligan <adammulligan@fastmail.fm>
2016-07-18 09:54:19 -0400

    Fixes bug in INSERT SQL when location is empty

--- app/src/services/cartoDBService.js
+++ app/src/services/cartoDBService.js
@@ -7,8 +7,17 @@ Mustache.escapeHtml = function (text) { return text; };
 
 const SELECT_SQL = 'SELECT ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng, details, email, created_at, name, title, visible, date, location, cartodb_id as id, media, user_id FROM {{{table}}} {{#id}} WHERE cartodb_id = {{{id}}} {{/id}} {{#userId}} WHERE user_id = \'{{{userId}}}\' {{/userId}} ORDER BY date ASC';
 const DELETE_SQL = 'DELETE FROM {{{table}}} WHERE cartodb_id = {{{id}}} LIMIT 1';
-const INSERT_SQL = 'INSERT INTO {{{table}}} (name, details, title, visible, location, email, date, user_id, media, the_geom) VALUES ({{{name}}}, {{{details}}},{{{title}}}, {{{visible}}}, {{{location}}},{{{email}}}, {{{date}}}, {{{userId}}}, {{{media}}},st_setsrid(ST_GeomFromGeoJSON({{{theGeom}}}),4326))' +
-    ' RETURNING ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng, details, email, created_at, name, title, visible, date, location, cartodb_id as id, media, user_id';
+const INSERT_SQL = `
+    INSERT INTO {{{table}}} (
+      name, details, title, visible, location, email, date, user_id,
+      media, the_geom
+    ) VALUES (
+      {{{name}}}, {{{details}}}, {{{title}}}, {{{visible}}},
+      {{{location}}}, {{{email}}}, {{{date}}}, {{{userId}}},
+      {{{media}}}, ST_SetSRID(ST_GeomFromGeoJSON({{{theGeom}}}), 4326)
+    ) RETURNING ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng, details,
+      email, created_at, name, title, visible, date, location, cartodb_id
+      as id, media, user_id`;
 
 var executeThunk = function(client, sql, params){
     return function(callback){
@@ -34,12 +43,12 @@ class CartoDBService {
     * createStory(story){
         var params = {
             name: wrapQuotes(story.name),
-            details: story.details ? wrapQuotes(story.details) : null,
+            details: story.details ? wrapQuotes(story.details) : 'null',
             title: wrapQuotes(story.title),
             visible: story.visible ? wrapQuotes(story.visible) : false,
-            location: story.location ? wrapQuotes(story.location) : null,
+            location: story.location ? wrapQuotes(story.location) : 'null',
             email: wrapQuotes(story.email),
-            date: story.date ? wrapQuotes(story.date) : null,
+            date: story.date ? wrapQuotes(story.date) : 'null',
             userId: story.userId ? wrapQuotes(story.userId) : 'null',
             media: wrapQuotes(JSON.stringify(story.media)),
             theGeom: wrapQuotes(JSON.stringify(story.geojson)),

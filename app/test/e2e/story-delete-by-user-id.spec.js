64461dc8b94fc1799ec5fe2593d38592a40b03eb
Rubén Vallejo <ruben.vallejo@vizzuality.com>
2022-10-06 11:09:01 +0200

    WIP: delete by userId test

--- app/test/e2e/story-delete-by-user-id.spec.js
+++ app/test/e2e/story-delete-by-user-id.spec.js
@@ -2,7 +2,9 @@ const nock = require('nock');
 const chai = require('chai');
 const config = require('config');
 const Story = require('models/story.model');
+const { USERS } = require('./utils/test.constants');
 const { getTestServer } = require('./utils/test-server');
+const { mockGetUserFromToken } = require('./utils/helpers');
 
 chai.should();
 
@@ -24,35 +26,60 @@ describe('Delete stories by user id', () => {
         await Story.remove({}).exec();
     });
 
-    it('Get story by id (happy case)', async () => {
+    it('Deleting a story by user Id without being logged should return a 401 Unauthorized', async () => {
+        const response = await requester
+            .delete(`/api/v1/story/by-user/${USERS.USER.id}`);
+
+        response.status.should.equal(401);
+    });
+
+    it('Deleting a story by user Id without being logged should return a 403 Forbidden', async () => {
+        mockGetUserFromToken(USERS.MANAGER);
+
+        const response = await requester
+            .delete(`/api/v1/story/by-user/${USERS.USER.id}`)
+            .set('Authorization', `Bearer abcd`);
+
+        response.status.should.equal(403);
+    });
+
+    it('Delete story by userId as ADMIN should return all stories deleted and a 200 status', async () => {
+        mockGetUserFromToken(USERS.ADMIN);
+
         nock(`https://${config.get('cartoDB.user')}.cartodb.com`, { encodedQueryParams: true })
-            .get('/api/v2/sql')
-            .query({
-                q: `SELECT ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng,
-                details, email, created_at, name, title, visible, date, location, cartodb_id as id,
-                media, user_id, hide_user FROM ${config.get('cartoDB.table')}  
-                WHERE cartodb_id = 131   ORDER BY date ASC`,
+            .post('/api/v2/sql', {
+                q: `
+    INSERT INTO ${config.get('cartoDB.table')} (
+      name, details, title, visible, location, email, date, user_id,
+      media, the_geom, hide_user
+    ) VALUES (
+      null, null, null, false,
+      null, null, null, '${USERS.USER.id}',
+      null, ST_SetSRID(ST_GeomFromGeoJSON('undefined'), 4326), false
+    ) RETURNING ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng, details,
+      email, created_at, name, title, visible, date, location, cartodb_id
+      as id, media, user_id, hide_user`,
                 api_key: config.get('cartoDB.apiKey'),
                 format: 'json'
             })
             .reply(200, {
                 rows: [{
-                    lat: 42.80887151387517,
-                    lng: -1.6471939086914222,
-                    details: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.\n\nHe lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.\n\nThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.\n\n"What\'s happened to me? " he thought. It wasn\'t a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.\n\nA collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.\n\nIt showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops',
+                    lat: 20.12345,
+                    lng: -48.23456,
+                    details: 'story details',
                     email: null,
-                    created_at: '2016-08-25T11:43:57Z',
+                    created_at: '2020-11-27T07:21:19Z',
                     name: null,
-                    title: 'Testing story from Pamplona and the user hide',
-                    visible: true,
-                    date: '2016-08-25T00:00:00Z',
-                    location: 'Pamplona, Municipio de Navarra',
-                    id: 131,
+                    title: 'story title',
+                    visible: false,
+                    date: '2020-01-01T00:00:00Z',
+                    location: 'location',
+                    id: 234,
                     media: '[{"previewUrl":"14721253102146a8c7ea386c528d2e90193f3f69d8e29nature_4.jpg","order":0},{"previewUrl":"147212530713536f9f59b55ca98097ed3a3e2c220220cnature_3.jpg","order":2},{"previewUrl":"14721253134877fd92494f7a0630ba37651dbc8c4bcf1nature_1.jpg","order":3},{"embedUrl":"youtube.com/watch?v=RtcrS7dmhcI","order":1}]',
-                    user_id: '57ab390ad1d5fb2f00b20e11',
+                    user_id: '1a10d7c6e0a37126611fd7a7',
                     hide_user: true
                 }],
-                time: 0.003,
+                time: 0.048,
                 fields: {
                     lat: { type: 'number', pgtype: 'float8' },
                     lng: { type: 'number', pgtype: 'float8' },
@@ -72,45 +99,21 @@ describe('Delete stories by user id', () => {
                 total_rows: 1
             });
 
+        nock(process.env.GATEWAY_URL)
+            .get(`/v1/user/${USERS.ADMIN.id}`)
+            .reply(200, {
+                data: {
+                    type: 'user',
+                    id: USERS.ADMIN.id,
+                    attributes: USERS.ADMIN
+                }
+            });
+
         const response = await requester
-            .get('/api/v1/story/131');
+            .delete(`/api/v1/story/by-user/${USERS.USER.id}`);
 
         response.status.should.equal(200);
-        response.body.data.should.be.an('object').and.deep.equal({
-            type: 'story',
-            id: '131',
-            attributes: {
-                name: null,
-                title: 'Testing story from Pamplona and the user hide',
-                createdAt: '2016-08-25T11:43:57.000Z',
-                visible: true,
-                details: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.\n\nHe lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.\n\nThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.\n\n"What\'s happened to me? " he thought. It wasn\'t a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.\n\nA collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.\n\nIt showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops',
-                date: '2016-08-25T00:00:00.000Z',
-                email: null,
-                location: 'Pamplona, Municipio de Navarra',
-                media: [
-                    {
-                        previewUrl: '14721253102146a8c7ea386c528d2e90193f3f69d8e29nature_4.jpg',
-                        order: 0
-                    },
-                    {
-                        previewUrl: '147212530713536f9f59b55ca98097ed3a3e2c220220cnature_3.jpg',
-                        order: 2
-                    },
-                    {
-                        previewUrl: '14721253134877fd92494f7a0630ba37651dbc8c4bcf1nature_1.jpg',
-                        order: 3
-                    },
-                    {
-                        embedUrl: 'youtube.com/watch?v=RtcrS7dmhcI',
-                        order: 1
-                    }
-                ],
-                lat: 42.80887151387517,
-                lng: -1.6471939086914222,
-                hideUser: true
-            }
-        });
+        response.body.data.should.have.lengthOf(2);
     });
 
     afterEach(async () => {

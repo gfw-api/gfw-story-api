791a08f5a12f3d6fd7bc9fe8ae3564390ab07a99
Rubén Vallejo <ruben.vallejo@vizzuality.com>
2022-10-06 16:05:42 +0200

    feat: adds endpoint to delete stories by user id, relevant tests

--- app/test/e2e/utils/helpers.js
+++ app/test/e2e/utils/helpers.js
@@ -1,11 +1,25 @@
 const nock = require('nock');
 
+const getRandomInRange = (to, from, fixed) => (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+
 const mockGetUserFromToken = (userProfile) => {
     nock(process.env.GATEWAY_URL, { reqheaders: { authorization: 'Bearer abcd' } })
         .get('/auth/user/me')
         .reply(200, userProfile);
 };
 
+const createStory = (additionalData = {}) => ({
+    details: 'test story',
+    title: 'Test Story',
+    location: 'Test location',
+    email: 'fakeEmail@potato.com',
+    name: 'fake Name',
+    lat: getRandomInRange(-90, 90, 2),
+    lng: getRandomInRange(-180, 180, 2),
+    ...additionalData
+});
+
 module.exports = {
-    mockGetUserFromToken
+    mockGetUserFromToken,
+    createStory,
 };

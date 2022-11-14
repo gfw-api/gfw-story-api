9da302d5870424092decf7799fb9a078de79792a
Rubén Vallejo <ruben.vallejo@vizzuality.com>
2022-11-14 17:44:38 +0100

    test: add multiple resources test

--- app/test/e2e/story-delete-by-user-id.spec.js
+++ app/test/e2e/story-delete-by-user-id.spec.js
@@ -114,10 +114,7 @@ describe('Delete stories by user id', () => {
             .set('Authorization', `Bearer abcd`);
         response.status.should.equal(200);
         response.body.data.should.have.lengthOf(2);
-        response.body.data[0].attributes.lat.should.equal(fakeStoryOne.toObject().lat);
-        response.body.data[0].attributes.lng.should.equal(fakeStoryOne.toObject().lng);
-        response.body.data[1].attributes.lat.should.equal(fakeStoryTwo.toObject().lat);
-        response.body.data[1].attributes.lng.should.equal(fakeStoryTwo.toObject().lng);
+        response.body.data.map((elem) => elem.attributes.lat).sort().should.deep.equal([fakeStoryOne.toObject().lat, fakeStoryTwo.toObject().lat].sort());
 
         const findStoriesByUser = await Story.find({ userId: { $eq: USERS.USER.id } }).exec();
         findStoriesByUser.should.be.an('array').with.lengthOf(0);
@@ -201,10 +198,7 @@ describe('Delete stories by user id', () => {
             .set('Authorization', `Bearer abcd`);
         response.status.should.equal(200);
         response.body.data.should.have.lengthOf(2);
-        response.body.data[0].attributes.lat.should.equal(fakeStoryOne.toObject().lat);
-        response.body.data[0].attributes.lng.should.equal(fakeStoryOne.toObject().lng);
-        response.body.data[1].attributes.lat.should.equal(fakeStoryTwo.toObject().lat);
-        response.body.data[1].attributes.lng.should.equal(fakeStoryTwo.toObject().lng);
+        response.body.data.map((elem) => elem.attributes.lat).sort().should.deep.equal([fakeStoryOne.toObject().lat, fakeStoryTwo.toObject().lat].sort());
 
         const findStoriesByUser = await Story.find({ userId: { $eq: USERS.USER.id } }).exec();
         findStoriesByUser.should.be.an('array').with.lengthOf(0);
@@ -288,10 +282,7 @@ describe('Delete stories by user id', () => {
             .set('Authorization', `Bearer abcd`);
         response.status.should.equal(200);
         response.body.data.should.have.lengthOf(2);
-        response.body.data[0].attributes.lat.should.equal(fakeStoryOne.toObject().lat);
-        response.body.data[0].attributes.lng.should.equal(fakeStoryOne.toObject().lng);
-        response.body.data[1].attributes.lat.should.equal(fakeStoryTwo.toObject().lat);
-        response.body.data[1].attributes.lng.should.equal(fakeStoryTwo.toObject().lng);
+        response.body.data.map((elem) => elem.attributes.lat).sort().should.deep.equal([fakeStoryOne.toObject().lat, fakeStoryTwo.toObject().lat].sort());
 
         const findStoriesByUser = await Story.find({ userId: { $eq: USERS.USER.id } }).exec();
         findStoriesByUser.should.be.an('array').with.lengthOf(0);
@@ -302,6 +293,57 @@ describe('Delete stories by user id', () => {
         const storyResourceTypes = findAllStories.map((story) => story._id.toString());
         storyResourceTypes.should.contain(fakeStoryFromManager._id.toString());
         storyResourceTypes.should.contain(fakeStoryFromAdmin._id.toString());
+    });
+
+    it('Deleting stories from a user should delete them completely from a database (large number of stories)', async () => {
+        mockGetUserFromToken(USERS.USER);
+
+        await Promise.all([...Array(25)].map(async () => {
+            const createdStory = await new Story(createStory({
+                userId: USERS.USER.id
+            })).save();
+
+            nock(`https://${config.get('cartoDB.user')}.cartodb.com`, { encodedQueryParams: true })
+                .post('/api/v2/sql', {
+                    q: `DELETE FROM ${config.get('cartoDB.table')} WHERE cartodb_id = ${createdStory.toObject()._id.toString()}`,
+                    api_key: config.get('cartoDB.apiKey'),
+                    format: 'json'
+                })
+                .reply(200, {
+                    rows: [{
+                        ...createdStory.toObject(),
+                    }],
+                    time: 0.003,
+                    fields: {
+                        lat: { type: 'number', pgtype: 'float8' },
+                        lng: { type: 'number', pgtype: 'float8' },
+                        details: { type: 'string', pgtype: 'text' },
+                        email: { type: 'string', pgtype: 'text' },
+                        created_at: { type: 'date', pgtype: 'timestamptz' },
+                        name: { type: 'string', pgtype: 'text' },
+                        title: { type: 'string', pgtype: 'text' },
+                        visible: { type: 'boolean', pgtype: 'bool' },
+                        date: { type: 'date', pgtype: 'timestamptz' },
+                        location: { type: 'string', pgtype: 'text' },
+                        id: { type: 'number', pgtype: 'int4' },
+                        media: { type: 'string', pgtype: 'text' },
+                        user_id: { type: 'string', pgtype: 'text' },
+                        hide_user: { type: 'boolean', pgtype: 'bool' }
+                    },
+                    total_rows: 1
+                });
+        }));
+
+        const deleteResponse = await requester
+            .delete(`/api/v1/story/by-user/${USERS.USER.id}`)
+            .set('Authorization', `Bearer abcd`)
+            .send();
+
+        deleteResponse.status.should.equal(200);
+        deleteResponse.body.should.have.property('data').with.lengthOf(25);
+
+        const findStoryByUser = await Story.find({ userId: { $eq: USERS.USER.id } }).exec();
+        findStoryByUser.should.be.an('array').with.lengthOf(0);
     });
 
     it('Deleting all stories of an user while being authenticated as USER should return a 200 and all stories deleted - no stories in the db', async () => {
